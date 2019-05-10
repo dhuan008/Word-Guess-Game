ef9f76ecba91df1008d945f245f361f4376246d8
David Huang <daveshuang@gmail.com>
2019-05-10 16:50:47 -0700

    added frame work and basic game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,65 @@
-// Variables
-var wordList = ["javascript", "functions", "float", "arrays", "strings"];
-var answerChars = [];
-var wins = 0;
+// Game object
+var wordGuessGame = {
+    // Variables
+    wordList: ["javascript", "functions", "float", "arrays", "strings"],
+    answerChars: [],
+    displayChars: [],
+    wrongGuesses: [],
+    playerInput: "",
+    wins: 0,
+    guesses: 12,
+
+    // Gets the answer and stores each character into the aanswerChars array
+    getWord: function () {
+        this.answerChars = (this.wordList[Math.floor(Math.random() * this.wordList.length)]).split("");
+        this.initGuesses();
+    },
+
+    // Helper function to initilize the current word display
+    initGuesses: function () {
+        this.displayChars.length = 0;
+        for (var i = 0; i < this.answerChars.length; i++) {
+            this.displayChars.push("_");
+        }
+        //console.log(this.answerChars);
+        //console.log(this.displayChars);
+    },
+
+    // Checks if player guess is correct. Inside the answerChars array
+    checkLetter: function (letter) {
+        for (var i = 0; i < this.answerChars.length; i++) {
+            if (letter == this.answerChars[i]) {
+                return true; // return letter
+            }
+        }
+        return false;
+    }
 
 
-function wordGuessGame() {
-    
-}
+};
 
-// Page loadeed
-document.addEventListener("DOMContentLoaded", function(event) {
+// Page loaded main function
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // Gets the word to guess
+    wordGuessGame.getWord();
+
     // On key press up
-    document.onkeyup = function(event) {
+    document.onkeyup = function (event) {
 
+        // If a letter is typed
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            wordGuessGame.playerInput = event.key.toLowerCase();
+            //console.log(wordGuessGame.playerInput);
 
-        var letter = event.key.toLowerCase();
+            //console.log(wordGuessGame.answerChars);
+
+            //console.log(wordGuessGame.checkLetter(wordGuessGame.playerInput));
+            if (wordGuessGame.checkLetter()) {
+
+            }
+        }
+
 
     }
 });
