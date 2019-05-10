621c4aaafd41b91dc1f768064eb36a4213f6f58f
David Huang <daveshuang@gmail.com>
2019-05-10 14:46:26 -0700

    updated interface

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// Variables
+var wordList = ["javascript", "functions", "float", "arrays", "strings"];
+var answerChars = [];
+var wins = 0;
+
+
+function wordGuessGame() {
+    
+}
+
+// Page loadeed
+document.addEventListener("DOMContentLoaded", function(event) {
+    // On key press up
+    document.onkeyup = function(event) {
+
+
+        var letter = event.key.toLowerCase();
+
+    }
+});
